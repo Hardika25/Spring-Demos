ef2c95fe499280288d63f8676026b0cc15e885f6
Hardika25 <51956314+Hardika25@users.noreply.github.com>
2019-07-23 16:50:29 +0530

    Add files via upload

---
+++ javaconfigdemo/src/main/java/com/cg/app/AppConfig.java
@@ -0,0 +1,17 @@
+package com.cg.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.cg.demo.EmployeeDAO;
+import com.cg.demo.EmployeeService;
+
+@Configuration
+@ComponentScan("com.cg.demo")
+public class AppConfig {
+
+	
+	
+	
+}
---
+++ javaconfigdemo/src/main/java/com/cg/demo/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.cg.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+	private EmployeeDAO dao;
+
+	public EmployeeDAO getDao() {
+		return dao;
+	}
+
+	@Autowired
+	public void setDao(EmployeeDAO dao) {
+		System.out.println("Performing Setter Injection");
+		this.dao = dao;
+		}
+		
+
+}
